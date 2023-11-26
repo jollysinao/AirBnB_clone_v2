b93eb861759fa02caf85c6304b8bd1f0ece0550a
jollysinao <jollysinao@gmail.com>
2023-11-26 05:24:38 -0800

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
+"""Initial script for undesrtand flask"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_hbnb():
-    """print web"""
-    return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
